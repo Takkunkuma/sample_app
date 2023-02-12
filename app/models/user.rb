bb923864f17687170b007a893694bdb2555b1dc6
Takkunkuma <tatsuokumamoto0330@gmail.com>
2023-02-12 23:36:10 +0000

    Setup AccountActivations Resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
-
-  before_save { email.downcase! }
-
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum: 50}
@@ -43,4 +42,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    #turning all email to downcase
+    def downcase_email
+      self.email.downcase!
+    end
+
+    # create and sub activation_token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
