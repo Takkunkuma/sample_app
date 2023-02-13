17ced62e295b6dfecfbf03e4222cc02c3d2ae1c1
Takkunkuma <tatsuokumamoto0330@gmail.com>
2023-02-13 05:57:17 +0000

    New branch after bug in email preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  host = "https://5d89837942a442cf91a39f2b156da930.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
